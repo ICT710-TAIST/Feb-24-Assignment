aa6cd78e9fa1e9d367c105c01d34638a496ffdbd
Kittikom Sangrit <sangrit@Sangrits-MacBook-Pro.local>
2020-02-25 11:53:15 +0700

    modified push-to-light-baremetal/main.cpp

--- push-to-light-baremetal/main.cpp
+++ push-to-light-baremetal/main.cpp
@@ -6,13 +6,7 @@ DigitalIn button(USER_BUTTON);
 int main()
 {
     while(true){
-        while(button) {
-            led = 1; // LED is ON
-            wait(0.2); // 200 ms
-            led = 0; // LED is OFF
-            wait(0.8); // 800 ms
-        }
-        led = 0; //LED is OFF
+       led = button; 
     }
 }
 
